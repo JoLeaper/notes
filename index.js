190c59ec7f8f3c625c1b592b833a0c7b0b5603d1
JoLeaper <joleap123@gmail.com>
2020-06-01 22:20:33 -0700

    modified functions and tests

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const notes = require('./lib/notes');
-const input = require('./lib/input');
+const { execute } = require('./lib/notes');
+const { parse, valid } = require('./lib/input');
+
+// console.log(parse(process.argv));
+
+// const thing = valid() 
+// console.log(thing);
+// execute(thing);
