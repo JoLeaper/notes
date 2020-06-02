90408bc1d27b0ad766c0795006240855626f3f9d
JoLeaper <joleap123@gmail.com>
2020-06-01 22:23:21 -0700

    running function successfully

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const { execute } = require('./lib/notes');
 const { parse, valid } = require('./lib/input');
 
-// console.log(parse(process.argv));
-
-// const thing = valid() 
-// console.log(thing);
-// execute(thing);
+const thing = valid(process.argv) && parse(process.argv);  
+execute(thing);
