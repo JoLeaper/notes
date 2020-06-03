2c2a6afe3ce116fe7b0699033cd69a85328dc2c2
JoLeaper <joleap123@gmail.com>
2020-06-03 16:35:21 -0700

    added mongoose to index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+const mongoose = require('mongoose');
+
 const Note = require('./lib/Notes');
 const { Input } = require('./lib/Input');
+
+mongoose.connect('mongodb://localhost:27017/play', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const action = new Input(process.argv);
 if (action.valid()) {
