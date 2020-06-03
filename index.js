ba2ef5b6fff54bf288c4a3178dcaa9ed56a40f1e
JoLeaper <joleap123@gmail.com>
2020-06-02 17:21:56 -0700

    capitalized class file names

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { Note } = require('./lib/notes');
-const { Input } = require('./lib/input');
+const { Note } = require('./lib/Notes');
+const { Input } = require('./lib/Input');
 
 const thing = new Input(process.argv);
 if (thing.valid()) {
